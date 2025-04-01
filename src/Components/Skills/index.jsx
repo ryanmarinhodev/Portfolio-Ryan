0d8da87fff69f9fa6788eee6e02c0b1b32762684
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-04-01 14:00:27 -0300

    Atualizando as hard skills

--- src/Components/Skills/index.jsx
+++ src/Components/Skills/index.jsx
@@ -1,37 +1,50 @@
-import './skills.css';
-import SkillsCard from './skillsCard';
+import "./skills.css";
+import SkillsCard from "./skillsCard";
 
-import logoFront from '../../img/logo-frontend1.png';
-import logoBack from '../../img/logo-backend.png';
-import logoFerramentas from '../../img/logo-ferramentas.png'
+import logoFront from "../../img/logo-frontend1.png";
+import logoBack from "../../img/logo-backend.png";
+import logoFerramentas from "../../img/logo-ferramentas.png";
 
 export default function Skills() {
-    return (
-        <section className='limitar-secao secao-skills'>
-            <h4>Tecnologias</h4>
-            <h2>Minhas Skills</h2>
-            <div className='secao-card-skills'>
-                <SkillsCard
-                    imagem={logoFront}
-                    descricao='Front-End'
-                    tecnologias={
-                        ['HTML', 'CSS', 'Javascript', 'TypeScript', 'React', 'Styled-Components', 'Material UI' 
-                        ]
-                    }
-                />
+  return (
+    <section className="limitar-secao secao-skills">
+      <h4>Tecnologias</h4>
+      <h2>Minhas Skills</h2>
+      <div className="secao-card-skills">
+        <SkillsCard
+          imagem={logoFront}
+          descricao="Front-End"
+          tecnologias={[
+            "React",
+            "NextJs",
+            "Javascript",
+            "TypeScript",
+            "Styled-Components",
+            "Material UI",
+            "HTML",
+            "CSS",
+          ]}
+        />
 
-                <SkillsCard
-                    imagem={logoBack}
-                    descricao='Back-End'
-                    tecnologias={['Node.js', 'Mongo db', 'Mysql', 'Sql']}
-                />
+        <SkillsCard
+          imagem={logoBack}
+          descricao="Back-End"
+          tecnologias={[
+            "Node.js",
+            "Express",
+            "MongoDB",
+            "Mysql",
+            "SQL",
+            "PostgreeSql",
+          ]}
+        />
 
-                <SkillsCard
-                    imagem={logoFerramentas}
-                    descricao='Ferramentas/Plataformas'
-                    tecnologias={['Git', 'GitHub']}
-                />
-            </div>
-        </section >
-    )
+        <SkillsCard
+          imagem={logoFerramentas}
+          descricao="Ferramentas/Plataformas"
+          tecnologias={["Git", "GitHub", "Postman", "Trello"]}
+        />
+      </div>
+    </section>
+  );
 }
